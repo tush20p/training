376b8c134bde1f796c7e499f1c1e1ce6c77486f7
tush20p <tush20p@gmail.com>
2019-07-15 18:29:40 +0530

    4_num modified the program ,remove goto

--- src/4_num.c
+++ src/4_num.c
@@ -1,49 +1,39 @@
 /* Run an infinite loop trying to accept user inputs, validate them for positivity. If number is invalid, don't process and go back to input request. If valid: check if it's even number add it to a variable which holds the sum of all even numbers input so far. If odd number, save it in another variable which always gets updated with odd numbers such that it has only the largest odd number entered so far. If user gives zero as input at any point, get out of the loop immediately and print the even-summation variable and the max odd number stored. Do not use 'else' anywhere in the code.*/
 
 #include"../inc/4_num.h"
-
 void main()
 {
-	int even=0,odd=0,num,count[100];
+	int even=0,odd=0,num=1;
 
-l1:do
-   {
-l2: printf("Enter the number\n");
-    scanf("%d",&num);
+	while(num!=0)
+	{
+		printf("Enter the number\n");
+		scanf("%d",&num);
 
-    printf("num=%d\n",num);
-    if(num<0)
-    {
-	    printf("Invalid input\n");
-	    goto l1;
-    }
+		printf("num=%d\n",num);
+		if(num<0)
+		{
+			printf("Invalid input\n");
+			continue;
+		}
 
-    if(num==0)
-    {
-	    printf("Sum of even no=%d\n",even);
-	    printf("max odd number=%d\n",odd);
-	    return;
-    }
-    while(num)
-    {
-	    if(num%2==0)
-	    {
-		    even=even+num;
-		    //		   printf("Sum of even no=%d\n",even);
-l3:			goto l2;
-	    }
 
-	    if(num%2!=0)
-	    {
-		    if(num>odd)
-		    {
-			    odd=num;
-			    //					printf("max odd number=%d\n",odd);
+		if(num%2==0)
+		{
+			even=even+num;
+			continue;
+		}
 
-		    }
-		    goto l3;
-	    }
-    }
+		if(num%2!=0)
+		{
+			if(num>odd)
+			{
+				odd=num;
+			continue;
 
-   }while(1);
+			}
+		}
+	}
+	printf("sum of even = %d \n largest of odd = %d\n",even,odd);
+
 }
