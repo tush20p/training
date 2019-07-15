30d541ee752e3b47dc4d59b7816c9bf8202adeeb
tush20p <tush20p@gmail.com>
2019-07-15 16:16:36 +0530

    3_train whole program changed as per review like header file and few changes

--- inc/3_train.h
+++ inc/3_train.h
@@ -3,3 +3,12 @@
 #include<string.h>
 #include<unistd.h>
 #include<time.h>
+typedef struct train
+{
+        char name[50];
+        char s[5];
+        int hh;
+        int mm;
+        int ss;
+}st;
+
--- src/3_train.c
+++ src/3_train.c
@@ -1,69 +1,102 @@
+/* Write a program to accepts name and arrival time of 5 trains and display the name with railway time format (2PM is written as 14:00). The information must be entered by the user. */
+
 #include"../inc/3_train.h"
-typedef struct train
-{
-	char name[50];
-	char s[5];
-	int hh;
-	int mm;
-	int ss;
-}st;
 
 int main()
 {
-	int i;
-	st train[2];
-	
-	
-	for(i=0;i<2;i++)
+	int i,ret=0;
+	st train[3];
+
+	for(i=0;i<3;i++)
 	{
-	printf("Enter the train name and time\n");
-	scanf("%s",train[i].name);
-		printf("Enter hours 'hh'     :\t");
-		scanf("%d",&train[i].hh);
+		printf("Enter the train name and time\n");
+		scanf("%s",train[i].name);
 
-		printf("Enter minutes 'mm'   :\t");
-		scanf("%d",&train[i].mm);
+l1:	printf("Enter hours 'hh'      :");
+	ret=scanf("%d",&train[i].hh);
 
-		printf("Enter seconds 'ss'   :\t");
-		scanf("%d",&train[i].ss);
-
-		printf("\n------------------------------\n\n");
-		
-		printf("Enter am or pm       :\t");
-		scanf("%s",train[i].s);
-	
-		printf("\n...............................\n\n");
-	}	
-	printf("\n===============================\n\n");
-
-	for(i=0;i<2;i++)
+	if(ret==0)
 	{
-			if(train[i].hh <= 12 && train[i].mm <= 59 && train[i].ss <= 59)      
-			{
-
-				if((strcmp(train[i].s,"PM") == 0) || (strcmp(train[i].s,"pm") == 0) 
-						&& (train[i].hh != 0) && (train[i].hh != 12))
-				{
-					train[i].hh = train[i].hh + 12;
-				}
-
-				if((strcmp(train[i].s,"AM") == 0) || (strcmp(train[i].s,"am") == 0) && (train[i].hh == 12))
-				{
-					train[i].hh = 0;
-				}
-
-				printf("train name:\t Train timimg : \t");
-				printf("%s- %02d:%02d:%02d",train[i].name,train[i].hh, train[i].mm, train[i].ss);
-
-				printf("\n\n");
-
-			}
-
-			else
-			{
-			printf("wrong input give the correct inputs.\n");
-			}
+		printf("Wrong input\n");
+		scanf("%*s");
+		goto l1;	
 	}
-	printf("=================================\n\n");
-	
+
+	else if((train[i].hh<1 || train[i].hh >12))
+	{
+		printf("Wrong input\n");
+		goto l1;	
+	}
+
+
+l2:	printf("Enter minute 'mm'     :");
+	ret=scanf("%d",&train[i].mm);
+
+	if(ret==0)
+	{
+		printf("Wrong input\n");
+		scanf("%*s");
+		goto l2;	
+	}
+
+	else if((train[i].mm<0 || train[i].mm >59))
+	{
+		printf("Wrong input\n");
+		goto l2;	
+	}
+
+	if(train[i].mm <= 59)
+	{
+		if((train[i].mm !=0 ) && (train[i].mm !=60 ))
+		{
+			train[i].mm = train[i].mm;
+		}
+	}
+
+l3:	printf("Enter Seconds 'ss'    :");
+	ret=scanf("%d",&train[i].ss);
+
+	if(ret==0)
+	{
+		printf("Wrong input\n");
+		scanf("%*s");
+		goto l3;	
+	}
+
+	else if((train[i].ss<0 || train[i].ss >59))
+	{
+		printf("Wrong input\n");
+		goto l3;	
+	}
+
+	if(train[i].ss <= 59)
+	{
+		if((train[i].ss !=0 ) && (train[i].ss !=59 ))
+		{
+			train[i].ss = train[i].ss;
+		}
+	}
+
+	printf("Enter the am or pm    :");
+	scanf("%s",train[i].s);
+
+
+	if((strcmp(train[i].s,"PM") == 0) || (strcmp(train[i].s,"pm") == 0) 
+			&& (train[i].hh != 0) && (train[i].hh != 12))
+	{
+		train[i].hh = train[i].hh + 12;
+	}
+
+	if((strcmp(train[i].s,"AM") == 0) || (strcmp(train[i].s,"am") == 0) && (train[i].hh == 12))
+	{
+		train[i].hh = 0;
+	}
+	printf("\n.........................................................\n");
+	printf("\tTrain Name:%s       |\t Train time: %02d:%02d:%02d",train[i].name,train[i].hh, train[i].mm, train[i].ss);
+	printf("\n.........................................................\n");
+
+	printf("\n\n");
+
+	}
+
 }
