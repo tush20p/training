71275e3305bdf69ed6aa63f54726e53bee1850c1
tush20p <tush20p@gmail.com>
2019-08-07 17:48:16 +0530

    modifiy program as per review comments 9_sum...

--- inc/9_sum.h
+++ inc/9_sum.h
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 int **my_malloc(int,int);
-void pointer_to_array(int r,int c,int **);
-void array_of_pointer(int,int,int **);
+int pointer_to_array(int r,int c,int **);
+int array_of_pointer(int,int,int **);
 void sum(int**,int**,int**,int,int);
 
--- src/9_sum.c
+++ src/9_sum.c
@@ -1,12 +1,8 @@
 /*write function to add two 2D arrays of variable dimension (dimension to be taken from user). Function should catch 1st array using pointer to array and 2nd using array of pointers.
   Print the resultant array using user defined printf() function which catches 2D array using double pointer. */
 
-/* TODO: Include only header name and not the whole path. */
-#include"../inc/9_sum.h"
+#include"9_sum.h"
 
-/* TODO: The program takes in 0 and 0 as a value of matrix rows and columns. This is wrong. Correct it..*/
-/* TODO: The program takes in negative value of matrix rows and columns. This is wrong. Correct it. */
-/* Please eliminate goto */
 
 int main()
 {
@@ -15,90 +11,101 @@ int main()
 
 	while(1)
 	{
-
 		while(ret==0)
 		{
 			printf("Enter the rows and column for matricx1:\n\n");
 			ret=scanf("%d",&r);
 			ret=scanf("%d",&c);
 			printf("row1=%d col1=%d\n\n",r,c);  
-			printf(".............................................\n\n");
-			if(ret==0)
+			if(r>0 && c>0)
 			{
-				scanf("%*s");
-				printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
-				ret=0;
-				continue;     
-			}
-
-
-			while(here==0)
-			{
-				printf("Enter the rows and column matrcsx2:\n\n");
-				here=scanf("%d",&l);
-				here=scanf("%d",&m);
-				printf("row2=%d col2=%d\n\n",l,m);  
 				printf(".............................................\n\n");
-
-				if(here==0)
+				if(ret==0)
 				{
-					printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
 					scanf("%*s");
-					here=0;
+					printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
+					ret=0;
+					continue;     
+				}
+
+
+				while(here==0)
+				{
+					printf("Enter the rows and column matrcsx2:\n\n");
+					here=scanf("%d",&l);
+					here=scanf("%d",&m);
+					printf("row2=%d col2=%d\n\n",l,m);  
+
+					if(l>0 && m>0)
+					{
+						printf(".............................................\n\n");
+
+						if(here==0)
+						{
+							printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
+							scanf("%*s");
+							here=0;
+							continue;
+						}
+
+						if(r == l && c==m)
+						{
+							p1=my_malloc(r,c);
+							p2=my_malloc(l,m);
+							p3=my_malloc(r,c);
+							pointer_to_array(r,c,p1);
+							array_of_pointer(r,c,p2);
+							sum(p1,p2,p3,r,c);
+							printf(".............................................\n");
+							printf("\n\n");
+							for(int i=0;i<r;i++)
+							{
+								printf("\t|");
+								for(int j=0;j<c;j++)
+									printf(" %d ",p1[i][j]);
+								printf("|\n");
+							}
+
+							printf("\t  __|__\n");
+							printf("\t    |  \n");
+
+							printf("\n\n");
+							for(int i=0;i<r;i++)
+							{
+								printf("\t|");
+								for(int j=0;j<c;j++)
+									printf(" %d ",p2[i][j]);
+								printf("|\n");
+							}
+
+							printf("=============================================\n\n");
+							printf("\n\n");
+							for(int i=0;i<r;i++)
+							{
+								printf("\t|");
+								for(int j=0;j<c;j++)
+									printf(" %d ",p3[i][j]);
+								printf("|\n");
+								exit(0);
+							}
+							break;
+						}
+						printf("wrong input..\n\n");
+						test=1;
+					}
+				printf("wrong input...\n");
+				here=0;
+				if(test==0)
 					continue;
 				}
-
-				if(r == l && c==m)
-				{
-					p1=my_malloc(r,c);
-					p2=my_malloc(l,m);
-					p3=my_malloc(r,c);
-					pointer_to_array(r,c,p1);
-					array_of_pointer(r,c,p2);
-					sum(p1,p2,p3,r,c);
-					printf(".............................................\n");
-					printf("\n\n");
-					for(int i=0;i<r;i++)
-					{
-						printf("\t|");
-						for(int j=0;j<c;j++)
-							printf(" %d ",p1[i][j]);
-						printf("|\n");
-					}
-
-					printf("\t  __|__\n");
-					printf("\t    |  \n");
-
-					printf("\n\n");
-					for(int i=0;i<r;i++)
-					{
-						printf("\t|");
-						for(int j=0;j<c;j++)
-							printf(" %d ",p2[i][j]);
-						printf("|\n");
-					}
-
-					printf("=============================================\n\n");
-					printf("\n\n");
-					for(int i=0;i<r;i++)
-					{
-						printf("\t|");
-						for(int j=0;j<c;j++)
-							printf(" %d ",p3[i][j]);
-						printf("|\n");
-					}
-					break;
-				}
-				printf("wrong input\n\n");
-				test=1;
 			}
+			printf("wrong input...\n");
 			ret=0;
 			if(test==0)
 				continue;
 		}
 	}
 }
-
 int **my_malloc(int r,int c)
 {
 	int **p;
@@ -108,39 +115,44 @@ int **my_malloc(int r,int c)
 	return p;
 }
 
-void pointer_to_array(int r,int c,int **p)
+int pointer_to_array(int r,int c,int **p)
 {
-	int ret;
-l2:printf("Enter the data\n");
-   for(int i=0;i<r;i++)
-	   for(int j=0;j<c;j++)
-		   ret=scanf("%d",&p[i][j]);
+	int ret=0,test=0;
 
-   if(ret==0)
-   {
-	   scanf("%*s");
-	   printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
-	   goto l2;
-   }
+do
+{
+	printf("Enter the data...1\n");
+	for(int i=0;i<r;i++)
+		for(int j=0;j<c;j++)
+			ret=scanf("%d",&p[i][j]);
 
+	if(ret==0)
+	{
+		scanf("%*s");
+		printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
+	}
+}while(ret==0);
+	
+}
+int array_of_pointer(int r,int c,int *p[])
+{
+	int ret=0,test=0;
+
+do
+{
+	printf("Enter the data...2\n");
+	for(int i=0;i<r;i++)
+		for(int j=0;j<c;j++)
+			ret=scanf("%d",&p[i][j]);
+
+	if(ret==0)
+	{
+		scanf("%*s");
+		printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
+	}
+}while(ret==0);
 }
 
-void array_of_pointer(int r,int c,int *p[])
-{
-	int ret;
-l2:printf("Enter the data\n");
-   for(int i=0;i<r;i++)
-	   for(int j=0;j<c;j++)
-		   ret=scanf("%d",&p[i][j]);
-
-   if(ret==0)
-   {
-	   scanf("%*s");
-	   printf("\n\nInvalid Option Entered...\nChoose Again...\n\n");
-	   goto l2;
-   }
-
-}
 
 void sum(int **p1,int **p2,int **p3,int r,int c)
 {
